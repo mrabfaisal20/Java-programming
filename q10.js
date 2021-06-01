ea74314626954433384657ca2215c01b5c305a1a
mrabfaisal20 <ab.fiqbal@gmail.com>
2021-06-01 23:10:36 +0500

    Add program 10

---
+++ q10.js
@@ -0,0 +1,21 @@
+const isPrime = num => {
+    for(var i = 2; i < num; i++){
+        if(num % i === 0) {return false;}
+  return num > 1;
+    }
+    
+  };
+  
+  for (let i = 0; i <= 30; i++) {
+    if (isPrime(i)) {
+      console.log(i,'is prime number', isPrime(i));
+    }
+    else if (!isPrime(i)){
+        if(i <3){
+        console.log(i,'is prime number', true);
+        }
+        else{
+            console.log(i,'is not prime number', isPrime(i));
+        }
+    }
+  }
