baa4132aeda3b4efdfea45d4b9de4bf7afdc9fa1
mrabfaisal20 <ab.fiqbal@gmail.com>
2021-06-01 22:54:24 +0500

    Add program 8

---
+++ q8.js
@@ -0,0 +1,11 @@
+const divisiblNum = num => {
+    if(num % 4 === 0){
+        return num
+    }
+  };
+  
+  for (let i = 0; i <= 100; i++) {
+    if (divisiblNum(i)) {
+      console.log(i, 'is divisible by 4');
+    }
+  }
