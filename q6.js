88d73e34f3d2e3771b26e06f1fcc4a6445e9f005
mrabfaisal20 <ab.fiqbal@gmail.com>
2021-06-01 22:48:20 +0500

    Solve program 7

--- q6.js
+++ q6.js
@@ -1,5 +1,5 @@
 let sum = 0
-for (let i = 0; i < 35; i++) {
+for (let i = 5; i < 35; i++) {
     sum += i;
 }
 console.log('sum of first 35 numbers', sum)
---
+++ q7.js
@@ -0,0 +1,3 @@
+let arr = [12, 23, 34 ,54, 79]
+let sortedArr = arr.sort()
+console.log('assending order', sortedArr)
